2384703e3f74358846f8bc4d4ad8d539813b813c
saif ali <saifalishaikh.email@gmail.com>
2022-02-05 19:30:08 +0530

    updates files

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,4 +5,20 @@ route.get('/',(req,res)=>{
     res.render('index')
 })
 
+route.get('/about',(req,res)=>{
+    res.render('about')
+})
+
+route.get('/portfolio',(req,res)=>{
+    res.render('portfolio')
+})
+
+route.get('/contacts',(req,res)=>{
+    res.render('contacts')
+})
+
+route.get('/blog',(req,res)=>{
+    res.render('blog')
+})
+
 module.exports = route
