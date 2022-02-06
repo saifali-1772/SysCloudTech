3036fe3762e27592743b4962c3fddb82248d7248
saif ali <saifalishaikh.email@gmail.com>
2022-02-06 17:46:41 +0530

    update fave icon or more files

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,23 +2,23 @@ const express = require('express')
 const route = express.Router()
 
 route.get('/',(req,res)=>{
-    res.render('index')
+    res.render('index',{header_data:""})
 })
 
 route.get('/about',(req,res)=>{
-    res.render('about')
+    res.render('about',{header_data:"| About"})
 })
 
 route.get('/portfolio',(req,res)=>{
-    res.render('portfolio')
+    res.render('portfolio',{header_data:"| Portfolio"})
 })
 
 route.get('/contacts',(req,res)=>{
-    res.render('contacts')
+    res.render('contacts',{header_data:"| Contacts"})
 })
 
 route.get('/blog',(req,res)=>{
-    res.render('blog')
+    res.render('blog',{header_data:"| Blog"})
 })
 
 module.exports = route
